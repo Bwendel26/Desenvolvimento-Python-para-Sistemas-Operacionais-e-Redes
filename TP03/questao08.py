9478b7816cdc8ccb92570fce4192739e641ca47d
bwendel26 <bwendel26999@gmail.com>
2021-03-15 23:43:43 -0300

    ...

---
+++ TP03/questao08.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+/************************ TESTE DE PERFORMANCE 03 **************************
+*    Questao 08                                                            *
+*        Aluno           : Francisco Alves Camello Neto                    *
+*        Disciplina      : Desenvolvimento Python para Redes e             *
+*                           Sistemas Operacionais                          *
+*        Professor       : Thaís do Nascimento Viana                       *
+*        Nome do arquivo : questao_08.py                                   *
+***************************************************************************/
+"""
+
+import os
+import time
+import socket
+import psutil
+import pickle
+from psutil._common import bytes2human
+
+
+msgFromClient = " Hello UDP Server"
+bytesToSend = msgFromClient.encode('ascii')
+
+serverAddressPort = (socket.gethostname(), 9991)
+bufferSize = 1024
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+try:
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    _msg = pickle.loads(msgFromServer[0])
+
+    templ = "%-17s %8s %8s %8s %5s%% %9s  %s"
+    print(templ % ("Device", "Total", "Used", "Free", "Use ", "Type",
+                    "Mount"))
+    for part in _msg:
+        if os.name == 'nt':
+            if 'cdrom' in part.opts or part.fstype == '':
+                # skip cd-rom drives with no disk in it; they may raise
+                # ENOENT, pop-up a Windows GUI error for a non-ready
+                # partition or just hang.
+                continue
+        usage = psutil.disk_usage(part.mountpoint)
+        print(templ % (
+            part.device,
+            bytes2human(usage.total),
+            bytes2human(usage.used),
+            bytes2human(usage.free),
+            int(usage.percent),
+            part.fstype,
+            part.mountpoint))
+
+except Exception as erro:
+            print(str(erro))
